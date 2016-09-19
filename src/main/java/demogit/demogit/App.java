52ea12ca4f4f51d5db82b2de78d0cdcca45af78f
CBT <CBT@CBT-118>
2016-09-19 06:07:27 +0530

    first commit

---
+++ src/main/java/demogit/demogit/App.java
@@ -0,0 +1,13 @@
+package demogit.demogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pack/Git.java
@@ -0,0 +1,17 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class Git {
+	
+	@Test
+	public void testGit(){
+		
+		System.out.println("This is the GitHub example program");
+		System.out.println("Start of PGM");
+		System.out.println("Hello everyone");
+		System.out.println("End of PGM");
+		
+	}
+
+}
